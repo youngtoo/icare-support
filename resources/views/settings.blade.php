2a3977409cf8b1e4f6dbeaafc1d0ab2eedbb72eb
Ian Too | #BeKind <ianktoo@gmail.com>
2022-10-06 08:09:19 +0300

    Created settings controller.

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -22,7 +22,11 @@
    @else
 
     @if ($option === 'components')
-        <p> Components </p>
+    <div class="widget-section p-3 bg-light rounded">
+        <h5>Manage Your Components</h5>
+        <p>This widget should be able to show ticket statuses.</p>
+        <button type="button" class="btn btn-sm btn-outline-dark" data-bs-toggle="popover" data-bs-title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Go To Tickets</button>
+    </div>
     @endif
        
    @endif
